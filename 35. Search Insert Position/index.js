264b20097beaa9bd2f01e6131f79be13fb0b277a
Douglas <douglasgodoy1998@hotmail.com>
2022-08-22 14:23:15 -0300

    chore

---
+++ 35. Search Insert Position/index.js
@@ -0,0 +1,33 @@
+// Binary Search
+
+var searchInsert = function (nums, target) {
+  let start = 0;
+  let end = nums.length;
+  let middle = Math.floor(end / 2);
+
+  if (!nums.includes(target)) {
+    let correctIndex = 0;
+    for (i = 0; i < nums.length; i++) {
+      if (target > nums[i]) {
+        correctIndex++;
+      }
+    }
+    return correctIndex;
+  } else {
+    while (start < end) {
+      if (target === nums[middle]) {
+        return middle;
+      }
+
+      if (target < nums[middle]) {
+        end = middle;
+        middle = Math.floor(end / 2);
+      } else if (target > nums[middle]) {
+        start = middle;
+        middle = start + ((end - start) >> 1);
+      }
+    }
+  }
+};
+
+// O(n)
