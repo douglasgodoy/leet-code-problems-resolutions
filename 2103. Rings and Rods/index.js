87cc8d3be3143e84afb1a79200b126529fef9dc8
Douglas <douglasgodoy1998@hotmail.com>
2022-08-22 14:11:41 -0300

    2103. Rings and Rods

---
+++ 2103. Rings and Rods/index.js
@@ -0,0 +1,28 @@
+var countPoints = function (rings) {
+  let hashTable = {},
+    rodsCompleted = 0;
+
+  for (i = 0; i < 10; i++) {
+    hashTable[i] = {
+      R: 0,
+      G: 0,
+      B: 0,
+    };
+  }
+
+  const splitStringInLetterAndNumber = rings.match(/\w[0-9]/gi);
+  splitStringInLetterAndNumber.forEach((string) => {
+    const [color, rod] = string.split("");
+    hashTable[rod][color] = 1;
+  });
+
+  for (let index in hashTable) {
+    const expected = JSON.stringify({ R: 1, G: 1, B: 1 });
+
+    if (JSON.stringify(hashTable[index]) === expected) {
+      rodsCompleted++;
+    }
+  }
+
+  return rodsCompleted;
+};
