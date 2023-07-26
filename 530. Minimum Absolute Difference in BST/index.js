8b2b46933262a7007c92af78ebad6ad8b0e0cc18
Douglas Godoy Cardoso <douglasgodoycardoso@Douglass-MacBook-Pro.local>
2023-07-25 22:49:10 -0300

    530. Minimum Absolute Difference in BST

--- 530. Minimum Absolute Difference in BST/index.js
+++ 530. Minimum Absolute Difference in BST/index.js
@@ -11,124 +11,55 @@
  * @return {number}
  */
 var getMinimumDifference = function (root) {
-  if (!root) return null;
+  let minimum = 100001;
 
-  const left = root.left ? root.left.val : 0;
-  const right = root.right ? root.right.val : 0;
-  if ((!left && !right) || root.val - left === 1) return root.val;
+  function dfs(node, prevVal) {
+    if (node) {
+      if (minimum === 1) return;
 
-  const leftValues = [];
-  const rightValues = [];
+      dfs(node.right, [...prevVal, node.val]);
+      dfs(node.left, [...prevVal, node.val]);
 
-  const recursiveValues = (node, array) => {
-    if (!node) return;
-    if (node.val !== null) {
-      array.push(node.val);
-      if (node.left !== null) {
-        recursiveValues(node.left, array);
-      }
+      prevVal.forEach((val) => {
+        let sub = node.val - val;
+        if (sub < 0) sub *= -1;
 
-      if (node.right !== null) {
-        recursiveValues(node.right, array);
-      }
+        minimum = Math.min(sub, minimum);
+      });
     }
-  };
+  }
 
-  recursiveValues(root.left, leftValues);
-  recursiveValues(root.right, rightValues);
+  if (root.right) {
+    if (root.right.val - root.val === 1) return 1;
+    dfs(root.right, [root.val]);
+  }
+  if (root.left) {
+    if (root.val - root.left.val === 1) return 1;
+    dfs(root.left, [root.val]);
+  }
 
-  const makeMath = (values) => {
-    let result = 100000;
-    for (const rootValue of values) {
-      for (const value of values) {
-        const currResult = rootValue - value;
-
-        if (result === null) {
-          result = currResult;
-        }
-
-        if (currResult === 1) {
-          return 1;
-        }
-        const subtract = currResult - result;
-        if (currResult < result && subtract > 0) {
-          result = currResult;
-        }
-      }
-      return result;
-    }
-  };
-
-  let resultLeft = makeMath(leftValues) || 100000;
-  let resultRight = makeMath(rightValues) || 100000;
-
-  return resultLeft < resultRight ? resultLeft : resultRight;
+  return minimum;
+};
+const tree = {
+  val: 236,
+  left: {
+    val: 104,
+    left: null,
+    right: {
+      val: 701,
+      left: {
+        val: 227,
+        left: null,
+        right: null,
+      },
+      right: {
+        val: 911,
+        left: null,
+        right: null,
+      },
+    },
+  },
+  right: null,
 };
 
-// console.log(
-//   getMinimumDifference({
-//     val: 20,
-//     left: {
-//       val: 15,
-//       left: { val: 7, left: null, right: null },
-//       right: { val: 3, left: null, right: null },
-//     },
-//     right: { val: 22, left: null, right: null },
-//   })
-// );
-
-console.log(
-  getMinimumDifference({
-    val: 1,
-    left: {
-      val: 0,
-      left: null,
-      right: null,
-    },
-    right: {
-      val: 48,
-      left: { val: 12, left: null, right: null },
-      right: { val: 49, left: null, right: null },
-    },
-  })
-);
-
-// console.log(
-//   getMinimumDifference({
-//     val: 4,
-//     left: {
-//       val: 2,
-//       left: {
-//         val: 1,
-//         left: null,
-//         right: null,
-//       },
-//       right: {
-//         val: 3,
-//         left: null,
-//         right: null,
-//       },
-//     },
-//     right: {
-//       val: 3,
-//       left: null,
-//       right: null,
-//     },
-//   })
-// );
-
-// console.log(
-//   getMinimumDifference({
-//     val: 1,
-//     left: null,
-//     right: {
-//       val: 3,
-//       left: {
-//         val: 2,
-//         left: null,
-//         right: null,
-//       },
-//       right: null,
-//     },
-//   })
-// );
+console.log(getMinimumDifference(tree));
