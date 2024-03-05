dbe449f70e71e24fc33d2c92566848ab25506328
Douglas Cardoso <douglasgodoy1998@hotmail.com>
2024-03-05 16:14:18 -0300

    5. Longest Palindromic Substring

---
+++ Medium/5. Longest Palindromic Substring/index.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function (s) {
+  let result = "";
+  for (let i = 0; i < s.length; i += 0.5) {
+    let center = i;
+    let left = Math.floor(center);
+    let right = Math.ceil(center);
+    let tmpPalidrome = "";
+
+    while (left >= 0 && right <= s.length) {
+      if (s[left] !== s[right]) {
+        tmpPalidrome = "";
+        break;
+      }
+
+      if (left === right) tmpPalidrome = s[left];
+      else tmpPalidrome = `${s[left]}${tmpPalidrome}${s[right]}`;
+
+      if (tmpPalidrome.length > result.length) result = tmpPalidrome;
+      left--;
+      right++;
+    }
+  }
+
+  return result;
+  6;
+};
+
+console.log(longestPalindrome("babadd"));
